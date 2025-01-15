6f619a27250a0435e4932247e9c72472ea318888
mkatay <kmagdi@kkando.hu>
2025-01-15 13:21:06 +0100

    add p tag

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,6 +4,7 @@ export const Banner = () => {
   return (
     <div className="banner">
     <h1>Pizza rendelés gyorsan, kényelmesen</h1>
+    <p>Valószínűleg ezt Te sem tudtad a pizzával kapcsolatban, de maga a pizza szó 997-ből származik. </p>
   </div>
   )
 }

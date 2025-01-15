28dea5808da2cfebb2afa18976b5972a890d6349
VezRoland <vezrolaa@gmail.com>
2025-01-15 13:26:03 +0100

    added title

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import { Products } from './components/Products';
 const queryClient = new QueryClient();
 
 function App() {
- 
   return (
     <QueryClientProvider client={queryClient}>
       <Banner />
--- src/components/SingleProducts.jsx
+++ src/components/SingleProducts.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const SingleProducts = () => {
   return (
     <div>
-      default SingleProducts
+      <h1>Single Products</h1>
     </div>
   )
 }
